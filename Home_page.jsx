7043f2421dd79f3287b187822dec24a56aa64060
aakashpatel7162 <48531888+aakashpatel7162@users.noreply.github.com>
2023-02-13 00:37:38 +0530

    Update Home_page.jsx

--- Home_page.jsx
+++ Home_page.jsx
@@ -3,6 +3,8 @@ import './upperRightnav.css'
 import './Midnav.css'
 import './rightLowerNav.css'
 import './Lowerleftnav.css'
+import { FaBeer } from 'react-icons/fa';
+
 
 
 export default function Homepge() {
